886043549081d60be8e7871f731715dd4dfb9d2d
quentinbastos <bastos.quentin.pro@gmail.com>
2026-02-24 17:28:08 +0100

    feat(tp2): act 3

--- tp2/Main_TP2_Bastos_Guillemain.C
+++ tp2/Main_TP2_Bastos_Guillemain.C
@@ -33,23 +33,50 @@ sbit Tst_P34 = P3^4;
 sbit Tst_P35 = P3^5;
 sbit Tst_P36 = P3^6;
 
+// Définition de la LED sur P1.6 (selon le schéma de la carte)
+sbit LED_VERTE = P1^6;
 
-//------------------------------------------------------------------------------------
-// MAIN Routine
-//------------------------------------------------------------------------------------
 void main (void) {
 
-	Init_Device();
-	XBR2 |= 0x04;  // Force le bit 2 (UART1E) à 1 pour activer l'UART1
-	Tst_P34 = 0;
-	LED = LED_Off;
-	
+	// --- INITIALISATION ---
+	Init_Device(); // Appelle la config de base (UART0, Timers, SYSCLK sur P1.3)
 
-   while (1)
-   {  
-   LED = ~LED;
-   Tst_P34 = ~Tst_P34;
-   tempo_sup('M',60);			 
-   }
+	// --- ACTIVITÉ 1 : Modification Crossbar ---
+	XBR2 |= 0x04;
+
+	P1MDOUT |= 0x40; // Met le bit 6 de P1MDOUT à 1 pour passer P1.6 en Push-Pull
+
+	// --- ACTIVITÉ 3 : Entrée Numérique (Bouton Poussoir) ---
+	/*
+	   PROBLÈME : Le bouton poussoir est une ENTRÉE. Pour lire une entrée sans
+	   ambiguïté, la broche doit être laissée en Open-Drain (par défaut) et on
+	   doit forcer la valeur à '1' dans le registre de sortie pour "ouvrir"
+	   le circuit de lecture.
+	*/
+	// Si le bouton est sur P3.7 par exemple :
+	P3 |= 0x80; // On écrit 1 sur le bit 7 pour pouvoir lire l'état réel externe
+
+	while (1) {
+
+
+		if (BOUTON == 0) {
+			LED_VERTE = 1;
+		} else {
+			LED_VERTE = 0;
+		}
+
+		{
+			unsigned long i;
+			for (i = 0; i < 50000; i++);
+		}
+	}
 }
 
+/* ACTIVITE 2 :
+ * On peut observer que la LED ne clignote pas sur la carte en premier temps.
+ * Ceci est du à la faible impudence qui fait que le signal n'est pas assez fort pour allumer la LED (de plus les ports du 8051 sont en open drain de base
+ * dans ce mode, la broche peut imposer un niveau bas donc 0V, elle ne peut pas pousser de courant à haut niveau donc haute impudence)..
+ * Cepandant, nous pouvons observer des signaux periodiques à l'aide de loscilataure, nous remarquons que la fréquence des
+ * signaux n'est pas compatible avec une fréquence observable.
+ * Pour allumer la LED, il faut modifier le mode de la broche en Push-Pull sur P1.6, grace à ca on a une tension sup a 2.2V en ignorant la résistance.
+ *
